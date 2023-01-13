5130ed441f542fdc9f44e8c63de931757d8b302a
Leonardo Nunes <leonardo-junior@users.noreply.github.com>
2023-01-13 19:47:28 -0300

    teste-send

--- packages/model/src/Model.tsx
+++ packages/model/src/Model.tsx
@@ -2,5 +2,5 @@ import * as React from "react"
 import { ModelDiv } from "./Model.styles"
 
 export const Model = () => {
-  return <ModelDiv>Modasdadelos</ModelDiv>
+  return <ModelDiv>Modasdaddelos</ModelDiv>
 }
