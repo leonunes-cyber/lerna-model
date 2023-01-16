45bf291eb084f150f3f7b385e30cd7c515772d0c
leonardo <leonardo.nunes@cybergenios.com.br>
2023-01-16 15:49:47 -0300

    teste

--- packages/model copy 2/src/Model.tsx
+++ packages/model copy 2/src/Model.tsx
@@ -2,5 +2,5 @@ import * as React from "react"
 import { ModelDiv } from "./Model.styles"
 
 export const Model = () => {
-  return <ModelDiv>Modelo 3</ModelDiv>
+  return <ModelDiv>Modeelo 3</ModelDiv>
 }
