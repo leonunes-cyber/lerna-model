7926595a027a485907b6642dbe3be947d57eafa0
leonardo <leonardo.nunes@cybergenios.com.br>
2023-01-16 11:33:53 -0300

    feat: add model 1 test

--- packages/model copy 2/src/Model.tsx
+++ packages/model copy 2/src/Model.tsx
@@ -2,5 +2,5 @@ import * as React from "react"
 import { ModelDiv } from "./Model.styles"
 
 export const Model = () => {
-  return <ModelDiv>Modasdasddelos</ModelDiv>
+  return <ModelDiv>Modelo 1</ModelDiv>
 }
