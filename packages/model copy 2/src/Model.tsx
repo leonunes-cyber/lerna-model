4fc3a965f73006dd3e3700c4af2ad2b7672e2c49
leonardo <leonardo.nunes@cybergenios.com.br>
2023-01-16 18:20:03 -0300

    feat: remove packages

--- packages/model copy 2/src/Model.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react"
-import { ModelDiv } from "./Model.styles"
-
-export const Model = () => {
-  return <ModelDiv>Modeelo 3</ModelDiv>
-}
--- packages/model copy 4/src/FinalModel.styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const ModelDiv = styled.div`
-  background-color: white;
-  color: red;
-`
